81b11ca98947f138919bfbb5111b3446ce5e0dd0
sabeeha shaik <142984283+sabeeha1234@users.noreply.github.com>
2026-04-15 10:58:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,28 @@
-//your code here
-  const allDivs = document.querySelectorAll(".image")
+const allDivs = document.querySelectorAll(".image")
 
-       
-       
-        for(let div of allDivs){
+for (let div of allDivs) {
 
+    div.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text", e.target.id)
+    })
 
+    div.addEventListener("dragover", (e) => {
+        e.preventDefault()
+    })
 
-            // Add drag start event for div
-            div.addEventListener("dragstart",(e)=>{
-                e.dataTransfer.setData("text",div.id)
-            })
+    div.addEventListener("drop", (e) => {
+        e.preventDefault()
 
+        const dropElement = e.currentTarget
 
-            // Preventing default behaviour of element to accept drops
-            div.addEventListener("dragover",(e)=>{
-                e.preventDefault()
-            })
+        const draggedElement = document.getElementById(
+            e.dataTransfer.getData("text")
+        )
 
+        const dropBg = getComputedStyle(dropElement).backgroundImage
+        const dragBg = getComputedStyle(draggedElement).backgroundImage
 
-            div.addEventListener("drop",(e)=>{
-                e.preventDefault()
-
-                const dropElement = e.target;
-                const dropElementBgImage = getComputedStyle(dropElement).backgroundImage
-
-                const draggedElement = document.getElementById(e.dataTransfer.getData("text"))
-                const draggedElementBgImage = getComputedStyle(draggedElement).backgroundImage
-
-                // console.log('dragged element image =>',draggedElementBgImage)
-                // console.log('drop element image =>',dropElementBgImage)
-
-                console.log("dragged element", draggedElement)
-                console.log('drop element',dropElement)
-
-
-                // Swap images
-                draggedElement.style.backgroundImage = dropElementBgImage
-                dropElement.style.backgroundImage = draggedElementBgImage
-            })
-
-        }
-
+        draggedElement.style.backgroundImage = dropBg
+        dropElement.style.backgroundImage = dragBg
+    })
+}
